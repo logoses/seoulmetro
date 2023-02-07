b2e6e0198f58e98093cd043f30bbe90d057051d3
holystorySeo <holystorseo@gmail.com>
2023-02-08 06:29:23 +0900

    amend: Fix dbquery for insert

--- server/app.js
+++ server/app.js
@@ -4,40 +4,55 @@ const port = 4000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const maria = require('mysql');
-const dbconfig = require('./database/connect');
+const maria = require("mysql");
+const dbconfig = require("./database/connect");
 const connection = maria.createConnection(dbconfig);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-        methods: ["GET", "POST"]
-    }));
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST"],
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("hi!")
-})
+  res.send("hi!");
+});
 
 app.post("/insert", (req, res) => {
-    console.log(req.body)
-    res.header("Access-Control-Allow-Origin", "*");
-})
+  for (let i = 0; i < line.length - 1; i++) {
+    connection.query(
+      "INSERT INTO SEOULMETRO.SUBWAY (line, snumber, sname, column1, column2, column3, column4, column5) VALUES",
+      (req.body.line[i],
+      req.body.snumber[i],
+      req.body.sname[i],
+      req.body.column1[i],
+      req.body.column2[i],
+      req.body.column3[i],
+      req.body.column4[i],
+      req.body.olumn5[i]),
+      (error, result) => {
+        if (error) {
+          return res.status(401).json({ message: "failure" });
+        } else {
+          res.status(200).json({ message: "success" });
+        }
+      }
+    );
+  }
+});
 
-app.get("/select", (req, res) => {
-    connection.query('SELECT * from subway', (error, rows) => {
-        if (error) throw error;
-        console.log('User info is: ', rows);
-        res.send(rows);
-    });
-
-})
+app.get("/list", (req, res) => {
+  connection.query("SELECT * FROM SUBWAY", (error, rows) => {
+    if (error) throw error;
+    console.log("User info is: ", rows);
+    res.send(rows);
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Connect at http://localhost:${port}`);
-})
-
-// const maria = require("./database/connect/maria")
-// maria.connect();
+  console.log(`Connect at http://localhost:${port}`);
+});
--- server/database/connect.js
+++ server/database/connect.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "1221@sion",
-    database: "seoulmetro"
-}
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "1221",
+  database: "seoulmetro",
+};
