c12c6228b86dc255d710c28cb980a5445ffc5cfb
logoses <logoses@mainlineit.com>
2023-02-08 17:37:23 +0900

    feat: Add insert query

--- server/app.js
+++ server/app.js
@@ -11,40 +11,58 @@ const connection = maria.createConnection(dbconfig);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-        methods: ["GET", "POST"],
-    })
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST"],
+  })
 );
 
 app.get("/", (req, res) => {
-    res.send("hi!");
+  res.send("hi!");
 });
 
 app.post("/insert", (req, res) => {
-    for (let i = 0; i < req.body.line.length - 1; i++) {
-        let sql = "INSERT INTO SUBWAY (line, snumber, sname, column1, column2, column3, column4, column5) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        let param = [req.body.line[i], req.body.snumber[i], req.body.sname[i], req.body.column1[i], req.body.column2[i], req.body.column3[i], req.body.column4[i], req.body.column5[i]]
+  for (let i = 0; i < req.body.line.length - 1; i++) {
+    let sql =
+      "INSERT INTO SUBWAY (line, snumber, sname, column1, column2, column3, column4, column5) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+    let param = [
+      req.body.line[i],
+      req.body.snumber[i],
+      req.body.sname[i],
+      req.body.column1[i],
+      req.body.column2[i],
+      req.body.column3[i],
+      req.body.column4[i],
+      req.body.column5[i],
+    ];
 
-        connection.query(sql, param, function (error, result) {
-            if (error) {
-                console.log("failure");
-            } else {
-                console.log("success");
-            }
-        })
-    }
+    connection.query(sql, param, function (error, result) {
+      if (error) {
+        console.log("failure");
+      } else {
+        console.log("success");
+      }
+    });
+  }
 });
 
 app.get("/list", (req, res) => {
-    connection.query("SELECT * FROM SUBWAY", (error, rows) => {
-        if (error) throw error;
-        console.log("User info is: ", rows);
-        res.send(rows);
-    });
+  connection.query("SELECT * FROM SUBWAY", (error, result) => {
+    if (error) throw error;
+
+    let headers = Object.keys(result[0]);
+    let values = [];
+
+    for (let i = 0; i < result.length - 1; i++) {
+      const value = Object.values(result[i]);
+      values.push(value);
+    }
+
+    res.json({ headers: headers, values: values });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Connect at http://localhost:${port}`);
+  console.log(`Connect at http://localhost:${port}`);
 });
