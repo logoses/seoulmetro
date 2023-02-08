3ebfec1ad28260a384696eded48c5eb53135907b
logoses <logoses@mainlineit.com>
2023-02-08 12:59:39 +0900

    amend: Fix dbQuery

--- server/app.js
+++ server/app.js
@@ -11,48 +11,40 @@ const connection = maria.createConnection(dbconfig);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-    methods: ["GET", "POST"],
-  })
+    cors({
+        origin: ["http://localhost:3000"],
+        credentials: true,
+        methods: ["GET", "POST"],
+    })
 );
 
 app.get("/", (req, res) => {
-  res.send("hi!");
+    res.send("hi!");
 });
 
 app.post("/insert", (req, res) => {
-  for (let i = 0; i < line.length - 1; i++) {
-    connection.query(
-      "INSERT INTO SEOULMETRO.SUBWAY (line, snumber, sname, column1, column2, column3, column4, column5) VALUES",
-      (req.body.line[i],
-      req.body.snumber[i],
-      req.body.sname[i],
-      req.body.column1[i],
-      req.body.column2[i],
-      req.body.column3[i],
-      req.body.column4[i],
-      req.body.olumn5[i]),
-      (error, result) => {
-        if (error) {
-          return res.status(401).json({ message: "failure" });
-        } else {
-          res.status(200).json({ message: "success" });
-        }
-      }
-    );
-  }
+    for (let i = 0; i < req.body.line.length - 1; i++) {
+        let sql = "INSERT INTO SUBWAY (line, snumber, sname, column1, column2, column3, column4, column5) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+        let param = [req.body.line[i], req.body.snumber[i], req.body.sname[i], req.body.column1[i], req.body.column2[i], req.body.column3[i], req.body.column4[i], req.body.column5[i]]
+
+        connection.query(sql, param, function (error, result) {
+            if (error) {
+                console.log("failure");
+            } else {
+                console.log("success");
+            }
+        })
+    }
 });
 
 app.get("/list", (req, res) => {
-  connection.query("SELECT * FROM SUBWAY", (error, rows) => {
-    if (error) throw error;
-    console.log("User info is: ", rows);
-    res.send(rows);
-  });
+    connection.query("SELECT * FROM SUBWAY", (error, rows) => {
+        if (error) throw error;
+        console.log("User info is: ", rows);
+        res.send(rows);
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Connect at http://localhost:${port}`);
+    console.log(`Connect at http://localhost:${port}`);
 });
--- server/database/connect.js
+++ server/database/connect.js
@@ -2,6 +2,6 @@ module.exports = {
   host: "localhost",
   port: "3306",
   user: "root",
-  password: "1221",
+  password: "1221@sion",
   database: "seoulmetro",
 };
